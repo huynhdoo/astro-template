945773571d3cf93cd180eec96805764ec27c493b
Do HUYNH <huynhdoo@gmail.com>
2023-04-05 16:10:24 +0200

    Add UnoCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import deno from "@astrojs/deno";
-
+import UnoCSS from 'unocss/astro'
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
-  integrations: [preact()],
+  integrations: [
+    preact(), 
+    UnoCSS({ /* options */ })
+  ],
 });
